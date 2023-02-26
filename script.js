ff51d5c022bacbaa5c105011324511067870687f
Juan Gotti <94028705+OneGoatee@users.noreply.github.com>
2023-02-26 13:50:54 -0500

    Refactor to make Grid Boxes Dimensions Dynamic

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 const container = document.querySelector('.container');
 const gridBox = document.createElement('div');
 const gridSize = 16;
+const totalGridBoxes = Math.pow(gridSize, 2);
+const gridBoxWidth = container.offsetWidth / gridSize;
+const gridBoxHeight = gridBoxWidth;
 
 gridBox.className = 'grid-box';
 
 // Function to add the Grid Boxes to the container
 function addGridBoxes(gridSize, targetElement) {
-  // Calculates number of boxes based on Grid Size
-  const totalBoxes = Math.pow(gridSize, 2);
-
   // Appends first Gird Box to the container
   targetElement.appendChild(gridBox);
 
   // Clone the original Grid Box to complete the grid based on the Grid Size
-  for (let i = 0; i < totalBoxes - 1; i++) {
+  for (let i = 0; i < totalGridBoxes - 1; i++) {
     targetElement.appendChild(gridBox.cloneNode(true));
   }
 }
@@ -23,10 +23,18 @@ function toggleBoxActiveState(e) {
   e.target.classList.toggle('grid-box-active');
 }
 
+function setGridBoxesDimensions(e) {
+  e.style.width = `${gridBoxWidth}px`;
+  e.style.height = `${gridBoxHeight}px`;
+}
+
 addGridBoxes(gridSize, container);
 
 const gridBoxes = document.querySelectorAll('.grid-box');
 
 gridBoxes.forEach(e => {
+  setGridBoxesDimensions(e);
   e.addEventListener('click', toggleBoxActiveState);
 });
+
+console.log('Width:', gridBoxWidth, 'Height:', gridBoxHeight);
