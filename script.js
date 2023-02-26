e47df917661cc45c534879105eb743945dfe2129
Juan Gotti <94028705+OneGoatee@users.noreply.github.com>
2023-02-26 13:28:04 -0500

    Add useful comments

--- script.js
+++ script.js
@@ -6,14 +6,19 @@ gridBox.className = 'grid-box';
 
 // Function to add the Grid Boxes to the container
 function addGridBoxes(gridSize, targetElement) {
+  // Calculates number of boxes based on Grid Size
   const totalBoxes = Math.pow(gridSize, 2);
+
+  // Appends first Gird Box to the container
   targetElement.appendChild(gridBox);
 
+  // Clone the original Grid Box to complete the grid based on the Grid Size
   for (let i = 0; i < totalBoxes - 1; i++) {
     targetElement.appendChild(gridBox.cloneNode(true));
   }
 }
 
+// Function to Toggle the Gid Box Active state when clicked
 function toggleBoxActiveState(e) {
   e.target.classList.toggle('grid-box-active');
 }
